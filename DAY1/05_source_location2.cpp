0cba78c1816d057565bd4144b9999f84937be82a
ecourse <smkang@codenuri.co.kr>
2023-07-03 10:13:10 +0900

    Add files via upload

--- DAY1/05_source_location2.cpp
+++ DAY1/05_source_location2.cpp
@@ -15,7 +15,6 @@ void log2(std::string_view msg,
 {													 
 	std::cout << loc.line() << " : " << msg << std::endl;
 }
-
 int main()
 {
 	log1("message1", __LINE__); // 15 : message1
@@ -26,6 +25,14 @@ int main()
 
 	log2("message1", std::source_location::current() );		
 	log2("message2");
+
+	// 위 코드를 정확히 이해 하려면 "디폴트 값"의 원리를 알아야 합니다.
+
+	// 아래 코드는 인자가 한개가 입니다.
+	log2("message3");	// => 컴파일러가 아래 코드로 변경합니다.
+						// log2("message3", std::source_location::current());
+						// 즉, current() 를 호출하는 것은 이 log2를 호출하는 
+						// 지점입니다.
 }
 
 
