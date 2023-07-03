95ddd5a82ced684e2b0e47b34e9e4cad7ed48d9d
ecourse <smkang@codenuri.co.kr>
2023-07-03 11:10:02 +0900

    Add files via upload

--- DAY1/06_lambda2-1.cpp
+++ DAY1/06_lambda2-1.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+int foo() { return 0; }
+
 int main()
 {
+	// 참고 : 평가되지 않은 표현식(unevaluated expression)
+	int n2 = sizeof( foo() );
+
+
+
 	// 1. C++20 전에는 람다표현식은 평가되지 않은 표현식에 넣을수 없었습니다.
 	int n = sizeof( [](int a, int b) { return a + b; } );
 
-	decltype([](int a, int b) { return a + b; }) f;
+	decltype( [](int a, int b) { return a + b; } ) f;
 }
