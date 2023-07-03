1a4e81eedf4279e91bf2b2ceedee8730263c4c7f
ecourse <smkang@codenuri.co.kr>
2023-07-03 16:15:04 +0900

    Add files via upload

--- DAY1/초기화.cpp
+++ DAY1/초기화.cpp
@@ -11,8 +11,16 @@ template<typename T, int N>
 struct array
 {
 	T buff[N];
+
+//	array(const array& other) {}
+//	array() {}
+
+	int size() const { return N; }
 };
+
 int main()
 {
 	array<int, 5> a = { 1,2,3,4,5 };
+			// => 이런 표기법이 되게 하려면 의도적으로
+			//	  어떠한 생성자도 만들지 말아야 합니다(복사 생성자, move 생성자등)
 }
