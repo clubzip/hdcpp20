1bdf3610c1c1f651a6bd784df88e33af5ead830f
ecourse <smkang@codenuri.co.kr>
2023-07-03 09:21:54 +0900

    Add files via upload

---
+++ DAY1/01_NEW_KEYWORD.cpp
@@ -0,0 +1,30 @@
+// C++20 에서 새로 추가된 키워드
+// Concept   지원  : concept, requires
+// Coroutine 지원  : co_await, co_return, co_yield
+// module    지원  : module, import, export
+// 상수성     지원  : constinit, consteval
+// UTF-8문자열 지원 :	char8_t
+
+// 기존 키워드에 의미가 추가된 경우
+// auto		: c++23
+// private	: C++20
+// if		: C++23
+// export	: C++20
+// this		: C++23
+
+
+// github.com/codenuri/hdcpp20  에서   
+// 
+// DAY1.zip 받으시면 됩니다.
+
+// 1. DAY1.zip 압축 풀고
+// 2. visual studio 에서 열어 보세요(day1.sln)
+// 
+// 3. 버전 설정해야 합니다.
+//    => 프로젝트 메뉴후 속성 메뉴 선택
+//    => C++언어 : 20으로 설정
+// 
+// 4. g++ 사용하실때는 -std=c++20 옵션 필요
+//   => g++ 소스이름.cpp  -std=c++20  ( -std=c++23)
+
+
