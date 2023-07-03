ab560e283751d3959b868eb6c6aab2a9a8ad0d8a
ecourse <smkang@codenuri.co.kr>
2023-07-03 10:38:39 +0900

    Add files via upload

--- DAY1/06_lambda1-1.cpp
+++ DAY1/06_lambda1-1.cpp
@@ -14,9 +14,17 @@ int main()
 	swap1(n, d); // 에러 아님..
 				 // generic lambda 는 2개의 인자가 서로 다른 템플릿!
 	
+
 	// 위 문제를 해결하기 위해서 C++20 부터는 람다표현식도 템플릿 가능합니다.
-	auto swap2 = []<typename T>(T & a, T & b) { auto t = a; a = b; b = t; };
+	auto swap2 = []<typename T>(T& a, T& b) { auto t = a; a = b; b = t; };
 
 	swap2(n, d); // error. 2개의 인자는 반드시 같은 타입이어야 합니다.
+
+	// 템플릿 인자 여러개 해도됩니다.
+	auto add1 = []<typename T1, typename T2>(T1 a, T2) { return a + b; };
+	auto add2 = [](auto a, auto b) { return a + b; }; // 위와 동일
 }
 
+// C++11 : 람다 표현식 도입
+// C++14 : generic 람다 표현식 등장
+// C++20 : template 람다 표현식 등장
