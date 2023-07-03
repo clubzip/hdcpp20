6e8ade2367d85cdd43a3117dcc2227e2cc33e8aa
ecourse <smkang@codenuri.co.kr>
2023-07-03 10:35:11 +0900

    Add files via upload

--- DAY1/06_lambda1-1.cpp
+++ DAY1/06_lambda1-1.cpp
@@ -11,6 +11,12 @@ int main()
 	double d = 3.4;
 
 	// 아래 코드는 되는게 좋을까요 ? 에러가 좋을까요 ?
-	swap1(n, d);
+	swap1(n, d); // 에러 아님..
+				 // generic lambda 는 2개의 인자가 서로 다른 템플릿!
+	
+	// 위 문제를 해결하기 위해서 C++20 부터는 람다표현식도 템플릿 가능합니다.
+	auto swap2 = []<typename T>(T & a, T & b) { auto t = a; a = b; b = t; };
+
+	swap2(n, d); // error. 2개의 인자는 반드시 같은 타입이어야 합니다.
 }
 
