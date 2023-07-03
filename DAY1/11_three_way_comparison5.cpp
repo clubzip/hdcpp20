d95b31547c798f72ee4d2c1923597fd94382792a
ecourse <smkang@codenuri.co.kr>
2023-07-03 15:47:30 +0900

    Add files via upload

--- DAY1/11_three_way_comparison5.cpp
+++ DAY1/11_three_way_comparison5.cpp
@@ -9,6 +9,12 @@ public:
 		: x(x), y(y), w(w), h(h)
 	{
 	}
+	// 사각형의 동일성, 순서 비교 를 어떻게 구현할 것인가요 ?
+	// => 면적으로 비교 하기로 했다!!
+	auto operator<=>(const Rect& r) const
+	{
+		return (w * h) <=> (r.w * r.h);
+	}
 };
 
 int main()
