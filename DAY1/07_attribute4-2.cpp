7ab1c9fd65a1a86c335e4cecbbb431a208be2c3f
ecourse <smkang@codenuri.co.kr>
2023-07-03 13:41:40 +0900

    Add files via upload

--- DAY1/07_attribute4-2.cpp
+++ DAY1/07_attribute4-2.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
 #include <type_traits>
 
+template<typename T1, typename T2, bool b = std::is_empty_v<T1>>
+struct PAIR
+{
+	T1 first;
+	T2 second;
+};
+// 3번째 인자가 true 인 경우를 위한 부분 특수화
+// 즉, PAIR 의 1번째 인자가 empty 인 경우를 위한 부분 특수화 입니다.
+template<typename T1, typename T2>
+struct PAIR<T1, T2, true> : public T1
+{
+	T2 second;
+};
+
+
+// C++에는 생각보다 empty 가 많습니다
+// => 대부분의 함수 객체
+// => 람다 표현식이 만드는 객체
+// => STL 의 allocator 
+// 등이 모두 empty class 입니다.
+
+struct Freer
+{
+	void operator()(void* p) noexcept
+	{
+		free(p);
+	}
+};
+
+
 // C++ 표준 스마트 포인터 unique_ptr 을 생각해 봅시다.
 // => 내부적으로 메모리 주소와 삭제자를 보관해야 합니다.
 
 template<typename T, typename D> 
 class unique_ptr
 {
-	T* addr;
-	D  del;   // 삭제자
+//	T* addr;
+//	D  del;   // 삭제자
+
+	PAIR<D, T*> pa;
+
 public:
-	unique_ptr(T* p) : addr(p) {}
+	unique_ptr(T* p) { pa.second = p; }
 
 	~unique_ptr()
 	{
-		del(p);
+		// 주소는 pa.second 에 보관 되고,
+		// 삭제자는 결국 pa의 기반 클래스이므로 operator()를
+		// 물려 받게 됩니다.
+		//del(addr);
+		pa.operator()(pa.second);
 	}
 };
 
