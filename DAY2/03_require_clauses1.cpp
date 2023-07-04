1aadf40f1ce1da54483a7d38c7a52f37e89f22c5
ecourse <smkang@codenuri.co.kr>
2023-07-04 15:15:38 +0900

    Add files via upload

--- DAY2/03_require_clauses1.cpp
+++ DAY2/03_require_clauses1.cpp
@@ -7,12 +7,22 @@ template<typename T>
 T gcd(T a, T b)
 {
 	static_assert(std::is_integral_v<T>, "error not integer");
-
 	return 0; // a, b의  최대 공약수를 구했다고 가정
+}
+
+double gcd(double a, double b)
+{
+	std::cout << "double" << std::endl;
+	return 0;
 }
 
 int main()
 {
 	gcd(10, 4);
 	gcd(3.4, 4.2); 
+
+	gcd(3.4f, 4.2f); // 핵심.!!! 인자가 float 타입이라면!!
+					 // => double 은 아니므로
+					 // => template 사용.. 그런데, static_assert
+					 //    때문에 실팽
 }
