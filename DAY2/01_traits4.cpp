baceb0c81b06ca8fd5dbcc4c0c42bd9dafa8466b
ecourse <smkang@codenuri.co.kr>
2023-07-04 11:09:25 +0900

    Add files via upload

--- DAY2/01_traits4.cpp
+++ DAY2/01_traits4.cpp
@@ -31,4 +31,29 @@ int main()
 {
 	int n = 0;
 	foo(&n);
+
+	// QA. const int& 에서 const 와 & 를 모두 제거하려면
+	// C++11
+	std::remove_const_t< std::remove_reference_t<const int&> > n1;
+	// => 정답
+
+	// 아래 코드는 그 아래의 포인터를 생각해보세요
+	std::remove_reference_t< std::remove_const_t<const int&> > n2;
+
+	// const int* const p;	// 앞 const : 가리키는 곳이 const
+							// 뒤 const : p가 const
+	std::remove_pointer_t< std::remove_const_t<const int* const>> n3;
+				// 뒤의 const 만 제거.. 
+
+	// reference 정체 : const pointer 입니다.
+
+	int& r1 = n;		// int* const r1 = &n 입니다.
+	const int& r2 = n;  // const int* const r2 = &n 입니다.
+
+	// C++20 에서는 remove_cvref 가 추가 되었습니다.
+	// remove_const : const 를제거
+	// remove_cv    : const, volatile 제거
+	// remove_reference : reference 제거
+	// remove_cvref : const, volatile, reference 제거 - C++20
+
 }
