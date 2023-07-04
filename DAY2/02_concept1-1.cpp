1a50c8429b6ab0e01bb13355868d63c4c0a72ac5
ecourse <smkang@codenuri.co.kr>
2023-07-04 11:27:01 +0900

    Add files via upload

--- DAY2/02_concept1-1.cpp
+++ DAY2/02_concept1-1.cpp
@@ -11,13 +11,21 @@
 //			concept 문법 사용
 //			"is_" 를 사용하지 말라고 권장
 
+
+
 template<typename T>
-concept container = requires(T c)
+//concept container = requires(T c) // 배열을 "값 타입(T)" 로 받으면 포인터로 받게 됩니다.
+concept container = requires(T& c)  // 이렇게 받으면 배열을 인자로 받을때 T가 배열 입니다.
 {
 	// 구현 방법 1. begin(), end() 멤버 함수가 있어야 한다.
-	c.begin();
-	c.end();
+	// c.begin();
+	// c.end();
+
+	// 구현방법 2. std::begin()에 인자로 전달 가능해야 한다.
+	std::begin(c);
+	std::end(c);
 };
+
 template<typename T> void check(T& c)
 {
 	// concept이름<타입> 하면 만족하는지 조사 가능합니다.
@@ -27,6 +35,7 @@ template<typename T> void check(T& c)
 
 int main()
 {
+	std::cout << std::boolalpha;
 	int n = 0;
 	std::vector<int> v = { 1,2,3 };
 	int x[3] = { 1,2,3 };
