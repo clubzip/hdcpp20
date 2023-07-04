5ce834fad0826acba0a3ff90443c9d2ff52625bd
ecourse <smkang@codenuri.co.kr>
2023-07-04 15:18:59 +0900

    Add files via upload

---
+++ DAY2/03_require_clauses2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+
+
+// template 안에서 static_assert 를 사용하면
+// => 조건을 만족하지 않으면 에러 발생!!
+
+// C++20. require clauses 를 사용한 템플릿
+// => 조건을 만족하지 않으면 사용하지 않겠다!!
+// => 동일 이름의 다른 함수가 있다면 사용가능
+
+template<typename T> requires std::is_integral_v<T>
+T gcd(T a, T b)
+{
+	return 0;
+}
+
+double gcd(double a, double b)
+{
+	std::cout << "double" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	gcd(10, 4);
+	gcd(3.4, 4.2);
+
+	gcd(3.4f, 4.2f); 
+}
