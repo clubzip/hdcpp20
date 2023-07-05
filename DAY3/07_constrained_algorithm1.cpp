92c6dde9a5ebb8db332f2575409ddc36c6ca7006
ecourse <smkang@codenuri.co.kr>
2023-07-05 15:38:49 +0900

    Add files via upload

--- DAY3/07_constrained_algorithm1.cpp
+++ DAY3/07_constrained_algorithm1.cpp
@@ -19,6 +19,21 @@ int main()
 		[](const std::string& s1, const std::string& s2) 
 					{ return s1.size() < s2.size(); });
 
+
+	// python 의 sort
+	// => 비교에 사용할 함수를 단항함수로 제공
+	// => 함수(e1) < 함수(e2) 로 비교!!
+	// std::sort(v.begin(), v.end(),
+	//	[](const std::string& s) { return s.size(); });
+
+
+	// C++20 의 선택
+	// => e1, e2 를 비교할때
+	// => 이항함수( 단항함수(e1), 단항함수(e2) )
+	// => 이때 키값을얻는 단항함수를 "projection" 이라고 합니다.
+	std::sort(v.begin(), v.end(), 이항비교함수, 키값을 얻는 단항함수);
+
+
 	for (const auto& e : v)
 	{
 		std::cout << e << ", ";
