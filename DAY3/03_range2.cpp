ea6fae305e462b516c718f8b7e23699bd0f50ef4
ecourse <smkang@codenuri.co.kr>
2023-07-05 11:08:41 +0900

    Add files via upload

---
+++ DAY3/03_range2.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+
+// 핵심 : 아래 take_view 의 기능을 잘 생각해 보세요
+template<typename T> 
+class take_view
+{
+	T& rng;
+	std::size_t sz;
+public:
+	take_view(T& r, std::size_t c) : rng(r), sz(c) {}
+
+	auto begin() { return rng.begin(); }
+	auto end()   { return rng.begin() + sz; }
+};
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+
+	take_view tv(v, 5); // 이 한줄을 잘생각해 보세요.
+
+	for (auto e : v)
+		std::cout << e << ", ";
+
+	std::cout << std::endl;
+
+
+}
