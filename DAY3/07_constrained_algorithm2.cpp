cab01b17ec2fc7bb1991e423adaab3d4aa36884c
ecourse <smkang@codenuri.co.kr>
2023-07-05 15:43:29 +0900

    Add files via upload

--- DAY3/07_constrained_algorithm2.cpp
+++ DAY3/07_constrained_algorithm2.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 
+// step 1. 기본 버전, 크기를 비교하는 정책을 변경할수 없습니다.
 template<typename T>
 const T& Max(const T& a, const T& b)
 {
@@ -12,7 +13,8 @@ int main()
 {
 	std::string s1 = "ABC";
 	std::string s2 = "XX";
+
 	auto ret = Max(s1, s2);
 
-	std::cout << ret << std::endl;
+	std::cout << ret << std::endl; // "XX"
 }
