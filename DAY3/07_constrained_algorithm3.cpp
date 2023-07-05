405447f781936a8b173616120ad83c76ce3bf29b
ecourse <smkang@codenuri.co.kr>
2023-07-05 15:50:14 +0900

    Add files via upload

--- DAY3/07_constrained_algorithm3.cpp
+++ DAY3/07_constrained_algorithm3.cpp
@@ -7,7 +7,8 @@
 template<typename T, typename Projection >
 const T& Max(const T& a, const T& b, Projection proj )
 {
-	return proj(a) < proj(b) ? b : a;
+//	return proj(a) < proj(b) ? b : a;
+	return std::invoke( proj, a) < std::invoke(proj, b) ? b : a;										// (b.*proj)()
 }
 
 int main()
@@ -15,8 +16,9 @@ int main()
 	std::string s1 = "ABC";
 	std::string s2 = "XX";
 
-	auto ret = Max(s1, s2, 
-		[](const std::string& s) { return s.size(); } );
+//	auto ret = Max(s1, s2, [](const std::string& s) { return s.size(); } );
 
-	std::cout << ret << std::endl; // "XX"
+	auto ret = Max(s1, s2, &std::string::size );
+
+	std::cout << ret << std::endl; // "ABC"
 }
