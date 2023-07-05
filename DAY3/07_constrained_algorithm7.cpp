81c62487e5667fc6e890a901aa84dfbfb5dc402d
ecourse <smkang@codenuri.co.kr>
2023-07-05 16:36:19 +0900

    Add files via upload

--- DAY3/07_constrained_algorithm7.cpp
+++ DAY3/07_constrained_algorithm7.cpp
@@ -11,9 +11,21 @@ int main()
 	std::vector<std::string> v = { "banana", "apple", "pear", "orange" };
 
 	// 핵심 1. 반복자 버전 vs 컨테이너 버전
-	std::ranges::sort(v.begin(), v.end()); // 반복자 버전
-	std::ranges::sort(v);				   // 컨테이너 버전
+//	std::ranges::sort(v.begin(), v.end()); // 반복자 버전
+//	std::ranges::sort(v);				   // 컨테이너 버전
 
+	// 핵심 2. 비교 함수 교체 - C++98 과 유사
+//	std::ranges::sort(v);  // 기본 버전 : std::less 사용 , < 로 비교
+//	std::ranges::sort(v, [](auto& s1, auto& s2) { return s1 > s2; });
+
+//	std::ranges::sort(v, std::greater{} ); // 위와 동일, > 연산을 하는 함수객체
+
+
+	// 핵심 3. projection 사용
+
+//	std::ranges::sort(v, std::less{}, &std::string::size );
+	std::ranges::sort(v, {}, &std::string::size);  // {} 만 전달해도 됨.
+											// 디폴트 타입이 less로 지정되었으므로
 
 
 	for (const auto& e : v)
