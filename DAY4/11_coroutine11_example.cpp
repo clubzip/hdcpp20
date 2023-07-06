d5908e2e0ece5d12944fb0ced358c31bc7a49509
ecourse <smkang@codenuri.co.kr>
2023-07-06 15:36:34 +0900

    Add files via upload

--- DAY4/11_coroutine11_example.cpp
+++ DAY4/11_coroutine11_example.cpp
@@ -87,17 +87,43 @@ public:
 
 		return *this;
 	}
-
-
-
-
-
-
 private:
 	std::coroutine_handle<promise_type> coro_handle;
+
+
+
+public:
+	// Generator 의 반복자
+	// => 결국 코루틴 핸들을 보관하다가 ++ 로 실행할수 있게 하면 됩니다.
+	class Iterator
+	{
+		std::coroutine_handle<promise_type> coro_handle;;
+	public:
+		Iterator(std::coroutine_handle<promise_type> h)
+			:coro_handle(h) {}
+
+		void operator++() { coro_handle.resume(); }
+
+		const T& operator*() { return coro_handle.promise().get_value(); }
+
+		bool operator==(const std::default_sentinel_t& s) const
+		{
+			return !coro_handle || coro_handle.done();
+		}
+	};
+
+	Iterator begin()
+	{
+		if (coro_handle)
+		{
+			coro_handle.resume();
+		}
+		return Iterator(coro_handle);
+	}
+
+	std::default_sentinel_t end() { return {}; }
 };
 //-----------------------------------
-
 Generator<int> range(int first, int last)
 {
 	while (first < last)
@@ -108,19 +134,26 @@ Generator<int> range(int first, int last)
 
 int main()
 {
+	// 파이썬의 range 를 만들어 봅시다.
+	// range 사용법 1. () 로 호출
 	auto g = range(15, 30);
 
 	std::cout << g() << std::endl; // 15
 	std::cout << g() << std::endl; // 16
 	std::cout << g() << std::endl; // 17
 
-
-	/*
-	// 파이썬의 range 를 만들어 봅시다.
+	
+	// 방법 2. range-for 에 넣기
+	// => range-for 원리를 생각하세요
+	
 	for (int e : range(15, 30) )	// 15 ~ 29 까지를 순차적으로 반환!
 	{
 		std::cout << e << ", ";
 	}
-	*/
+	
+//	auto obj = range(15, 30);
+//	auto first = obj.begin();
+//	auto last = obj.end();
+
 
 }
