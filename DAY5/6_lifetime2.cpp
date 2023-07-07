4be40060982bf69b442c02fc6db60ac7573a1db3
ecourse <smkang@codenuri.co.kr>
2023-07-07 13:41:41 +0900

    Add files via upload

--- DAY5/6_lifetime2.cpp
+++ DAY5/6_lifetime2.cpp
@@ -24,7 +24,16 @@ int main()
 //	String s2 = "BB";
 //	const String& s3 = mymax(s1, s2); // ok.. 안전한 코드입니다.
 
+	// 핵심 : 임시객체의 수명은 "최초에 가리키는 const reference" 에 의해서만
+	//		  연장될수 있습니다.
+	//		  2번 수명연장은 될수 없습니다.
+	//		  아래 s3 는 사용할수 없습니다. "dangling" 입니다.
+	//        cppreference.com 에서 "std::max" 검색
 	const String& s3 = mymax(String("AA"), String("BB")); // ?
 
+	std::cout << "==========" << std::endl;
+
 	std::cout << s3 << std::endl;
+
+	std::cout << "finish main" << std::endl;
 }
