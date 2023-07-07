8b9fefdfa389254f47d1dfa7c6a86fec2883e156
ecourse <smkang@codenuri.co.kr>
2023-07-07 15:07:30 +0900

    Add files via upload

--- DAY5/fo1.cpp
+++ DAY5/fo1.cpp
@@ -3,10 +3,25 @@
 #include <vector>
 #include <algorithm>
 
+bool foo(int n)
+{
+	return n % 3 == 0;
+}
+
 int main()
 {
 	std::vector v = { 1,6,3,7,8 };
 
 	// 1. 주어진 구간에서 3을 찾고 싶다.
 	auto ret1 = std::find(v.begin(), v.end(), 3);
+
+	// 2. 주어진 구간에서 "3의 배수" 를 찾고 싶다.
+	auto ret2 = std::find_if(v.begin(), v.end(), foo);
+
+
+	// 3. 주어진 구간에서 "k의 배수" 를 찾고 싶다.
+
+	int k = 3; // 사용자가 입력한 값이라고 생각해 보세요 
+
+	auto ret3 = std::find_if(v.begin(), v.end(), ? );
 }
