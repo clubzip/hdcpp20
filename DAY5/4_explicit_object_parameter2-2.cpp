e39e38253bcec48773c7ae1a915e02fd9fd7f45f
ecourse <smkang@codenuri.co.kr>
2023-07-07 10:29:21 +0900

    Add files via upload

---
+++ DAY5/4_explicit_object_parameter2-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Sample
+{
+public:
+	// 핵심 : 동일이름의 함수를 const, non-const 로 동시에 
+	//       만들수 있습니다.
+	void f1()       { std::cout << "f1" << std::endl; }
+	void f1() const { std::cout << "f1 const" << std::endl; }
+};
+int main()
+{
+	Sample s;
+	const Sample cs;
+
+	s.f1();
+	s.f2();
+}
