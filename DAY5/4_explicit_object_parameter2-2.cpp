a5c95eb359c7f79ef1bd73a28a6b75cedebec1be
ecourse <smkang@codenuri.co.kr>
2023-07-07 10:31:59 +0900

    Add files via upload

--- DAY5/4_explicit_object_parameter2-2.cpp
+++ DAY5/4_explicit_object_parameter2-2.cpp
@@ -13,6 +13,10 @@ int main()
 	Sample s;
 	const Sample cs;
 
-	s.f1();
-	cs.f1();
+	s.f1();	// 상수 객체가 아닌경우
+			// 1. non-const 를 사용합니다.
+			// 2. non-const 버전이 없다면 const 버전을 사용합니다.
+
+	cs.f1(); // 상수 객체인 경우
+			 // => const 버전만 사용가능 합니다.("f1 const")
 }
