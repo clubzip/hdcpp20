2d4f0be5fc247f9f56569eb3301ae1a4abae5261
ecourse <smkang@codenuri.co.kr>
2023-07-07 11:46:19 +0900

    Add files via upload

--- DAY5/QA.cpp
+++ DAY5/QA.cpp
@@ -18,14 +18,24 @@ public:
 
 
 	      T& operator[](int idx)       { return buff[idx]; }
+
+
+	// 아래 2줄의 차이점을 생각해 봅시다.
+	// T 반환    : v[0] 번째 메모리에 있는 값 반환.      rvalue(임시객체)
+	// const T& : v[0] 번째 메모리의 별명을 상수로 반환. lvalue
 	const T& operator[](int idx) const { return buff[idx]; }
 
+//		   T operator[](int idx) const { return buff[idx]; }
 };
 
 
 int main()
 {
 	const vector v(10, 3);
+
+	// & 연산자는 lvalue 에만 사용가능합니다.
+	// rvalue(임시객체)는 주소를 구할수 없습니다.
+	std::cout << &v[0] << std::endl;
 
 
 }
