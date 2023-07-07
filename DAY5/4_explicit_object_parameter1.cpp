4b0bbfe2fe8911a46481a32d44f44f882863bac9
ecourse <smkang@codenuri.co.kr>
2023-07-07 09:50:38 +0900

    Add files via upload

--- DAY5/4_explicit_object_parameter1.cpp
+++ DAY5/4_explicit_object_parameter1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 // C++23 의 Big 1 : Explicit Object Parameter
+// => 흔히 별명으로 "Deducing this" 라고 부릅니다.
 
 class Sample
 {
@@ -11,6 +12,7 @@ public:
 	{
 		std::cout << "f1" << std::endl;
 	}
+
 	// C++23 부터 객체를 명시적으로 받는 문법이 도입됩니다.
 	// => 아래 코드에서 "self" 는 변수 이름입니다. 아무이름이나 상관없습니다.
 	// => Python 과 유사한 형태입니다.
