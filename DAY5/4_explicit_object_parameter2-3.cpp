3629ee2c8bbdaba7467d206ddb3b223de160464a
ecourse <smkang@codenuri.co.kr>
2023-07-07 10:50:32 +0900

    Add files via upload

--- DAY5/4_explicit_object_parameter2-3.cpp
+++ DAY5/4_explicit_object_parameter2-3.cpp
@@ -16,8 +16,35 @@ public:
 	}
 	~vector() { delete[] buff; }
 
+	// vector 의 [] 연산자는 결국 아래 처럼 2개 만들어야 합니다.
+	// => C++ 에서 이처럼 동일 이름의 함수를 "const", "non-const"로
+	//    모두 제공하는 경우가 아주 많습니다
+	//    v.begin() 도 아래 처럼 2개 만들어야 합니다.
+//	      T& operator[](int idx)       { return buff[idx]; }
+//	const T& operator[](int idx) const { return buff[idx]; }
 
-	T& operator[](int idx) { return buff[idx]; }
+
+	/*
+	// 위 2개를 explicit object paramter 로 해봅시다.
+	T& operator[](this vector& self, int idx)
+	{ 
+		return self.buff[self.idx]; 
+	}
+
+	// 핵심 : 함수() 뒤에 있던 const 가 함수 인자가 되었습니다.
+	const T& operator[](this const vector& self, int idx)
+	{
+		return self.buff[self.idx];
+	}
+	*/
+	// 위 2개 함수는 구현이 완전히 동일한데 인자의 타입만 다릅니다.
+	// => 템플릿으로 하면 한개의 함수로.. 위 2개를 자동생성 가능합니다.
+	template<typename U>
+	auto& operator[](this U& self, int idx)
+	{
+		return self.buff[self.idx];
+	}
+	
 };
 
 int main()
@@ -33,11 +60,11 @@ int main()
 	vector v1(10, 3);
 	const vector v2(10, 3);
 
-	//	되야할까요 ?   원하는것		현재상태
-	int n1 = v1[0];	//
-	int n2 = v2[0];	//
+	//	되야할까요 ?     원하는것		
+	int n1 = v1[0];	// O
+	int n2 = v2[0];	// O
 
-	v1[0] = 5;		//
-	v2[0] = 5;		// 
+	v1[0] = 5;		// O
+	v2[0] = 5;		// X
 }
 
