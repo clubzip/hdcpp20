23ca44a66f8199d3942a1278d822717d0b77d1e4
ecourse <smkang@codenuri.co.kr>
2023-07-07 10:26:15 +0900

    Add files via upload

--- DAY5/4_explicit_object_parameter2-1.cpp
+++ DAY5/4_explicit_object_parameter2-1.cpp
@@ -21,8 +21,20 @@ public:
 	// 위 2개의 멤버 함수를 "explicit object parameter" 문법으로 
 	// 만들어 보세요
 
-
+	void set(this Point& self, int a, int b)
+	{
+		self.x = a;
+		self.y = b;
+	}
+	// 기존의 const - member 함수를 
+	// "explicit object parameter" 로 만들때는 const 가 인자 위치에 표기합니다.
+	void print(this const Point& self)
+	{
+		std::cout << self.x << ", " << self.y << std::endl;
+	}
 };
+
+
 int main()
 {
 	Point pt(1, 1);  
